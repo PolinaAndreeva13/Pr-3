e21bcac37acef416ea201446ae6ced5bbf603e46
PolinaAndreeva13 <polya3192@yandex.ru>
2023-04-30 22:13:17 +0700

    Добавлена таблица для корзины

--- script2.js
+++ script2.js
@@ -1,33 +1,41 @@
-let keys = Object.keys(localStorage), i = 1;
+let keys = Object.keys(localStorage), i = 1, j = 1;
 
-for(let key of keys) {
-    let basket,res = [0,0];
-    if (key != 'endSum' && key != 'endCount'){
+for (let key of keys) {
+    let basket, res = [0, 0];
+    if (key != 'endSum' && key != 'endCount') {
         let div = document.createElement("div"),
-        p1 = document.createElement("p"),
-        p2 = document.createElement("p"),
-        p3 = document.createElement("p");
-        div.id = `${i}`;
+            p1 = document.createElement("p"),
+            p2 = document.createElement("p"),
+            p3 = document.createElement("p");
         basket = localStorage.getItem(key);
         res = JSON.parse(basket);
         p1.className = "nameOfClothBasket"
-        p1.style = "margin-right: 320px;"
         p2.className = "nameOfClothBasket"
         p3.className = "nameOfClothBasket"
-        p2.style = "margin-right: 300px;"
         p1.innerHTML += key;
         p2.innerHTML += res[0];
         p3.innerHTML += res[1];
-        div.appendChild(p1);
-        div.appendChild(p2);
-        div.appendChild(p3);
-        div.className = "forDivTwoHTML";
-        document.body.append(div);
-        i++;
-    }  
-    else if (key == 'endSum'){
+        let table = document.getElementById("table_");
+        let tr = document.createElement("tr");
+        tr.id = "tr_";
+        let td1 = document.createElement("td");
+        let td2 = document.createElement("td");
+        let td3 = document.createElement("td");
+        td1.id = `${i}`;
+        td2.id = `${i}`;
+        td3.id = `${i}`;
+        td1.appendChild(p1);
+        td2.appendChild(p2);
+        td3.appendChild(p3);
+        tr.appendChild(td1);
+        tr.appendChild(td2);
+        tr.appendChild(td3);
+        table.appendChild(tr);
+        j++;
+    }
+    else if (key == 'endSum') {
         let div = document.createElement("div"),
-        p = document.createElement("p");
+            p = document.createElement("p");
         div.id = `${i}`;
         p.className = "forSUMandSUMCOUNTTwoHTML";
         p.innerHTML += localStorage.getItem(key) + ' × ';
@@ -36,9 +44,9 @@ for(let key of keys) {
         document.body.append(div);
         i++;
     }
-    else{
+    else {
         let div = document.createElement("div"),
-        p = document.createElement("p");
+            p = document.createElement("p");
         div.id = `${i}`;
         p.className = "forSUMandSUMCOUNTTwoHTML2";
         p.innerHTML += localStorage.getItem(key);
@@ -49,11 +57,11 @@ for(let key of keys) {
     }
 }
 
-function Clear(){
-    for(let key of keys) {
+function Clear() {
+    for (let key of keys) {
         localStorage.removeItem(key);
     }
-    for (let j = 1;; j++){
-        document.getElementById(`${j}`).innerHTML = ' ';
+    for (let a = 0; a < j; a++){
+        document.getElementById("tr_").remove();
     }
 }
